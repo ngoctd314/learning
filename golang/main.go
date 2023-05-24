dc9b65fd78089141726964839b37b0c406456623
tranngoc <tranngoc.contact@gmail.com>
2023-05-25 00:37:49 +0700

    add string

--- golang/main.go
+++ golang/main.go
@@ -1,63 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
-
-type gender int8
-
-// enum for gender
-const (
-	MALE gender = iota
-	FEMALE
-	OTHER
-)
-
-type person struct {
-	age      int
-	name     string
-	location int
-	gender   gender
-}
-type filterPersonFunc func(person) bool
-
-func filterPeople(people []person, filters ...filterPersonFunc) []person {
-	result := make([]person, 0)
-	matchAllFilter := func(person person, filters ...filterPersonFunc) bool {
-		for _, fitler := range filters {
-			if !fitler(person) {
-				return false
-			}
-		}
-		return true
-	}
-
-	for _, person := range people {
-		if matchAllFilter(person, filters...) {
-			result = append(result, person)
-		}
-	}
-
-	return result
-}
-
-func withPersonFilterByEqualAge(age int) filterPersonFunc {
-	return func(p person) bool {
-		return p.age == age
-	}
-}
-func withPersonFilterByGreaterThanAge(age int) filterPersonFunc {
-	return func(p person) bool {
-		return p.age > age
-	}
-}
-func withPersonFilterByLocation(location int) filterPersonFunc {
-	return func(p person) bool {
-		return p.location == location
-	}
-}
+import "fmt"
 
 func main() {
-	aString := "Ngoctd"
-	fmt.Sprintf("%p", &aString)
+	var hello = []byte("Hello")
+	var world = []byte("World")
+
+	helloWorld := append(hello, world...)
+	fmt.Println(string(helloWorld))
+
+	helloWorld2 := make([]byte, len(hello)+len(world))
+	copy(helloWorld2, hello)
+	copy(helloWorld2[len(hello):], world)
+
+	fmt.Println(string(helloWorld2))
 }
