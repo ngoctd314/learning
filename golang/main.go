23566642cff1c47bcd66bb8a186a0f09aa55d454
tranngoc <tranngoc.contact@gmail.com>
2023-05-29 23:20:08 +0700

    add func

--- golang/main.go
+++ golang/main.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	var hello = []byte("Hello")
-	var world = []byte("World")
 
-	helloWorld := append(hello, world...)
-	fmt.Println(string(helloWorld))
-
-	helloWorld2 := make([]byte, len(hello)+len(world))
-	copy(helloWorld2, hello)
-	copy(helloWorld2[len(hello):], world)
-
-	fmt.Println(string(helloWorld2))
+	var fn func()
+	fn = nil
+	fn()
 }
