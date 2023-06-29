d25cd4af2603508324e00c05f836ebd3594f798a
tranngoc <tranngoc.contact@gmail.com>
2023-06-29 08:03:52 +0700

    add redis

---
+++ database/redis/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	exampleClient()
+}
+
+func exampleClient() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-master.system:6379",
+		Username: "default",
+		Password: "custombot@#",
+		DB:       0,
+	})
+	ctx := context.Background()
+	// status := rdb.Ping(ctx)
+	// log.Println(status)
+	log.Println(rdb.HSet(ctx, key, "hello", "hello1"))
+	log.Println(rdb.HGetAll(ctx, key))
+}
+
+const key = "user_online"
