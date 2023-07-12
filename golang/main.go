1f2e4fa9e56001110ab15aa525fe9e97684f196c
ngoctd <tranngoc.contact@gmail.com>
2023-07-12 15:28:42 +0700

    exhausted

--- golang/main.go
+++ golang/main.go
@@ -1,46 +1,28 @@
 package main
 
 import (
-	"log"
-	"time"
+	"fmt"
+	"unsafe"
 )
 
-type person struct {
-	age  int
-	name string
+type sample struct {
+	a int
+	b string
 }
-
-// T1 ...
-type T1 struct {
+type t1 struct {
 	a int8
-	// On 64-bit architectures, to make field b 8-byte aligned, 7 bytes need to be padded here
-	// On 32-bite architectures, to make field b 4-byte aligned, 3 byes need to be padded here
 	b int64
-	c int16
-	// To make the size of type T1 be a multiple of the alignment guarantee of T1
-	// On 64-bit architectures, 6 bytes need to be padded here, on 32-bit architecture,
-	// 2 bytes need to be padded here
-	t2 T2
+	c int64
 }
 
-// T2 ...
-type T2 struct {
-	a int8 // 1 byte align
-	// On 64-bit architectures, to make field c 2-byte aligned, one byte needs to be padded here on both 64-bit
-	// and 32-bit architectures.
-	c int16 // 2 byte align
-	// On 64-bit architectures, to make field b 8-byte aligned, 4 bytes need to be padded
-	// here.  On 32-bit architectures, field b is already 4-byte aligned, so no bytes need to be padded here.
+type t2 struct {
+	a int8
+	c int16
 	b int64
 }
 
 func main() {
-	t := time.NewTicker(time.Second * 2)
-	defer t.Stop()
-
-	select {
-	case <-t.C:
-		log.Println("RUN")
-	}
-
+	s := &sample{a: 1, b: "test"}
+	startAddress := uintptr(unsafe.Pointer(s))
+	fmt.Printf("Start address of s: %d\n", startAddress)
 }
--- oauth2/docs/JWT for OAuth 2.0 Access Tokens RFC9068.md
+++ oauth2/docs/JWT for OAuth 2.0 Access Tokens RFC9068.md
@@ -2,8 +2,10 @@
 
 ## Abstract
 
-This specification defines a profile for issuing OAuth 2.0 access tokens in JSON Web Token (JWT) format.
+This specification defines a profile for issuing OAuth 2.0 access tokens in JSON Web Token (JWT) format. Authorization servers and 
 
 ## Introduction
 
 The original OAuth 2.0 Authorization Framework specification does not mandate any specific format for access tokens.
+
+While that remains perfectly appropriate for many important scenarios
