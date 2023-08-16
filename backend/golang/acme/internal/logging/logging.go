c080ce3a2da8774bceff9811e47e08d4efdaa800
tranngoc <tranngoc.contact@gmail.com>
2023-08-16 08:48:50 +0700

    u

---
+++ backend/golang/acme/internal/logging/logging.go
@@ -0,0 +1,21 @@
+package logging
+
+import "fmt"
+
+type LoggerStdOut struct{}
+
+func (l LoggerStdOut) Debug(message string, args ...any) {
+	fmt.Printf("[DEBUG] "+message, args...)
+}
+
+func (l LoggerStdOut) Info(message string, args ...any) {
+	fmt.Printf("[INFO] "+message, args...)
+}
+
+func (l LoggerStdOut) Warn(message string, args ...any) {
+	fmt.Printf("[WARN] "+message, args...)
+}
+
+func (l LoggerStdOut) Error(message string, args ...any) {
+	fmt.Printf("[ERROR] "+message, args...)
+}
