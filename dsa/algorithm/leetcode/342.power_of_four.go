e83cf2a2135efd93742cb6060a882bb4837e2a40
ngoctd <tranngoc.contact@gmail.com>
2023-09-04 20:13:07 +0700

    recursion

---
+++ dsa/algorithm/leetcode/342.power_of_four.go
@@ -0,0 +1,16 @@
+package main
+
+func isPowerOfFour(n int) bool {
+	return true
+}
+
+func isPowerOfFourRecursion(n int) bool {
+	if n == 1 {
+		return true
+	}
+	if n <= 0 || n%4 != 0 {
+		return false
+	}
+
+	return isPowerOfFourRecursion(n / 4)
+}
---
+++ dsa/algorithm/leetcode/342.power_of_four_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func Test_isPowerOfFour(t *testing.T) {
+}
+
+func Test_isPowerOfFourRecursion(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  int
+		want bool
+	}{
+		{
+			name: "Test 1",
+			arg:  16,
+			want: true,
+		},
+		{
+			name: "Test 2",
+			arg:  5,
+			want: false,
+		},
+		{
+			name: "Test 3",
+			arg:  1,
+			want: true,
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.want != isPowerOfFourRecursion(tt.arg) {
+			t.Errorf("%s, want %v", tt.name, tt.want)
+		}
+	}
+}
