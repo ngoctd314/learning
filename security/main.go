f925a4341dfd0b6402af88817e468d3dac2cf407
ngoctd <tranngoc.contact@gmail.com>
2023-09-27 00:17:13 +0700

    add secure

--- security/main.go
+++ security/main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"context"
-	"security/owasp/cmdinjection"
-
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-)
-
-var (
-	db  *sqlx.DB
-	ctx context.Context
-)
-
 func main() {
-	cmdinjection.APIServer()
 }
