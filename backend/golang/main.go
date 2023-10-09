9fa3e96baa28bb8ba86941b13b10a53caba92a9a
ngoctd <tranngoc.contact@gmail.com>
2023-10-09 19:15:17 +0700

    add go mistake

--- backend/golang/main.go
+++ backend/golang/main.go
@@ -1,22 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 func main() {
-	for i := 0; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
-		}()
-	}
+	i := 0
+	go func() {
+		i++
+	}()
+	go func() {
+		i++
+	}()
 }
 
-// func printAlloc() {
-// var m runtime.MemStats
-// ReadMemStats populates m with memory allocator statistic
-// The returned memory allocator statistics are up to date as of the
-// call to ReadMemStats. This is in constrast with a heap profile
-// which is a snapshot as of the most recently completed garbage
-// collection cycle.
-// runtime.ReadMemStats(&m)
-// fmt.Printf("%d KB\n", m.Alloc/1024)
-// }
+func sequentialVer() (int64, float64) {
+	now := time.Now()
+	var rs int64 = 0
+	for i := 0; i < 1e9; i++ {
+	}
+	for i := 0; i < 1e9; i++ {
+	}
+	return rs, time.Since(now).Seconds()
+}
+
+func concurrentVer() (int64, float64) {
+	now := time.Now()
+	wg := sync.WaitGroup{}
+	var rs atomic.Int64
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		var i int64 = 0
+		for ; i < 1e9; i++ {
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		var i int64 = 0
+		for ; i < 1e9; i++ {
+		}
+	}()
+	wg.Wait()
+
+	return rs.Load(), time.Since(now).Seconds()
+}
+
+func printAlloc() {
+	var m runtime.MemStats
+	// ReadMemStats populates m with memory allocator statistic
+	// The returned memory allocator statistics are up to date as of the
+	// call to ReadMemStats. This is in constrast with a heap profile
+	// which is a snapshot as of the most recently completed garbage
+	// collection cycle.
+	runtime.ReadMemStats(&m)
+	fmt.Printf("%d KB\n", m.Alloc)
+}
