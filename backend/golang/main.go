e80713d9873e234745b96b125869f66540b1309f
ngoctd <tranngoc.contact@gmail.com>
2023-10-25 20:00:55 +0700

    add go mistake on time.Time marshal and unmarshal

--- backend/golang/main.go
+++ backend/golang/main.go
@@ -3,38 +3,33 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"runtime"
 	"time"
 )
 
 type Event struct {
-	ID int `json:"id"`
-	time.Time
-}
-
-type foo struct{}
-
-func (foo) MarshalJSON() ([]byte, error) {
-	return []byte(`"foo"`), nil
+	Time time.Time
 }
 
 func main() {
-	b, err := json.Marshal(foo{})
+	t := time.Now()
+	event1 := Event{
+		Time: t,
+	}
+	b, err := json.Marshal(event1)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Println(string(b))
-
-	e := Event{
-		ID:   1234,
-		Time: time.Now(),
+	var event2 Event
+	err = json.Unmarshal(b, &event2)
+	if err != nil {
+		log.Fatal(err)
 	}
-	data, _ := json.Marshal(e)
-	fmt.Println(string(data))
-}
+	fmt.Println(event1, event2)
 
-func (e Event) MarshalJSON() ([]byte, error) {
-	return []byte(`"ABC"`), nil
+	fmt.Println(event1 == event2)
+	fmt.Println(event1.Time.Equal(event2.Time))
 }
 
 func printAlloc() {
