43e001116ad30815728a5431b659f0b7fd4a051f
ngoctd <tranngoc.contact@gmail.com>
2023-11-16 21:46:26 +0700

    u

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,6 +1,18 @@
-text = (
-    "Put several strings within parentheses"
-    "to have them joined together"
-    "to have them joined together"
-)
-print(text)
+from enum import Enum
+
+
+class Color(Enum):
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+
+
+color = Color(input("Enter your choice of 'red', 'blue' or 'green'"))
+
+match color:
+    case Color.RED:
+        print("I see Red")
+    case Color.GREEN:
+        print("I see Green")
+    case Color.BLUE:
+        print("I see Blue")
