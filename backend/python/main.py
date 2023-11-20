cd934bda086d3ed77dbc618cf3466861ec411a3c
ngoctd <tranngoc.contact@gmail.com>
2023-11-20 22:51:08 +0700

    ds python

--- backend/python/main.py
+++ backend/python/main.py
@@ -1,5 +1,17 @@
-def greeting(name: str, age: int) -> None:
-    print(f"Annotations: {greeting.__annotations__}")
+a = []
 
+a.append(1)
+print(a)
+a[len(a) :] = [2]
+print(a)
 
-greeting("ngoctd", 23)
+a.extend(range(3, 5))
+print(a)
+a[len(a) :] = [5, 6]
+print(a)
+
+a.insert(0, -1)
+print(a)
+
+a.remove(-1)
+print(a)
