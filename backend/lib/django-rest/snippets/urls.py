1cb797d28c0ed44d07e1744bb48895d492d3b8e3
ngoctd <tranngoc.contact@gmail.com>
2023-11-27 21:25:49 +0700

    rm st

--- backend/lib/django-rest/snippets/urls.py
+++
@@ -1,8 +0,0 @@
-from snippets import views
-from django.urls import path
-
-
-urlpatterns = [
-    path("snippets/", views.SnippetList.as_view()),
-    path("snippets/<int:pk>", views.SnippetDetail.as_view()),
-]
