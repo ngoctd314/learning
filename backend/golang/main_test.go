412d6ef2760fea7428e6286d30ca4aa8943ddf4c
ngoctd <tranngoc.contact@gmail.com>
2023-12-18 13:32:32 +0700

    add go false sharing

--- backend/golang/main_test.go
+++ backend/golang/main_test.go
@@ -30,3 +30,56 @@ func Benchmark_popcnt(b *testing.B) {
 	}
 	global = v
 }
+
+func Benchmark_count1(b *testing.B) {
+	var n int64 = 1000000
+	var inputs []Input
+
+	inputs = make([]Input, 0, n)
+	for i := int64(0); i < n; i++ {
+		inputs = append(inputs, Input{
+			a: 1,
+			b: 1,
+		})
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		count1(inputs)
+	}
+}
+
+func Benchmark_count2(b *testing.B) {
+	var n int64 = 1000000
+	var inputs []Input
+
+	inputs = make([]Input, 0, n)
+	for i := int64(0); i < n; i++ {
+		inputs = append(inputs, Input{
+			a: 1,
+			b: 1,
+		})
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		count2(inputs)
+	}
+}
+func Benchmark_count3(b *testing.B) {
+	var n int64 = 1000000
+	var inputs []Input
+
+	inputs = make([]Input, 0, n)
+	for i := int64(0); i < n; i++ {
+		inputs = append(inputs, Input{
+			a: 1,
+			b: 1,
+		})
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		count3(inputs)
+	}
+}
